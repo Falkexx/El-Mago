fc6e498fb57f7d4b4bdf93e4aa217b24bd370912
thalles-victor <thalles.victor2012gmail.com>
2024-11-11 13:16:31 -0300

    :sparkles: feat: configure the typeorm in the project.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { env } from './utils';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: env.POSTGRES_HOST,
+      port: env.POSTGRES_PORT,
+      username: env.POSTGRES_USER,
+      password: env.POSTGRES_PASSWORD,
+      database: env.POSTGRES_DB,
+      entities: [],
+      synchronize: false,
+      logging: 'all',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
