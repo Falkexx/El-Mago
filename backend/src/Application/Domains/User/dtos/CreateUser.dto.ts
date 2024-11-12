44537cf05740de3ce9be34e69bf03c150f2efe81
thalles-victor <thalles.victor2012@gmail.com>
2024-11-12 18:00:33 -0300

    :lock: sec: add class validator.

--- backend/src/Application/Domains/User/dtos/CreateUser.dto.ts
+++ backend/src/Application/Domains/User/dtos/CreateUser.dto.ts
@@ -1,4 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsNotEmpty()
+  @Matches(/^(?=.*[A-Z])(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-z]).{8,15}$/, {
+    message:
+      'A senha deve ter 8 carecteres, um especial um maúsculo e um número no mínimo',
+  })
   password: string;
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,12 @@ import 'dotenv/config';
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning();
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
