5aa72acd901224f90cf20827a8d6ff10bd275981
thalles-victor <thalles.victor2012@gmail.com>
2024-11-12 17:31:26 -0300

    :sparkles: feat: implement signIn method.

--- backend/src/Application/Domains/Auth/Auth.controller.ts
+++ backend/src/Application/Domains/Auth/Auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from '../User/dtos/CreateUser.dto';
 import { AuthService } from './Auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller({ path: 'auth', version: '1' })
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('signUp')
   async signUp(@Body() userDto: CreateUserDto) {
     return this.authService.signUp(userDto);
+  }
+
+  @Post('signIn')
+  async signIn(@Body() authDto: AuthDto) {
+    return this.authService.sinIn(authDto);
   }
 }
--- backend/src/Application/Domains/Auth/Auth.service.ts
+++ backend/src/Application/Domains/Auth/Auth.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../User/User.service';
 import { PayloadType } from '#types';
 import { CreateUserDto } from '../User/dtos/CreateUser.dto';
 import { JwtService } from '@nestjs/jwt';
+import { AuthDto } from './dto/auth.dto';
+import { UserEntity } from 'src/Application/Entities/User.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -14,12 +17,7 @@ export class AuthService {
   async signUp(userDto: CreateUserDto) {
     const user = await this.userService.create(userDto);
 
-    const payload: PayloadType = {
-      sub: user.id,
-      roles: [user.role],
-    };
-
-    const token = this.jwtService.sign(payload);
+    const token = await this.generateToken(user);
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, ..._user } = user;
@@ -28,5 +26,43 @@ export class AuthService {
       user: _user,
       accessToken: token,
     };
+  }
+
+  async sinIn(authDto: AuthDto) {
+    const user = await this.userService.getUserBy({ email: authDto.email });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    const isThePasswordCorrect = await bcrypt.compare(
+      authDto.password,
+      user.password,
+    );
+
+    if (!isThePasswordCorrect) {
+      throw new UnauthorizedException();
+    }
+
+    const token = await this.generateToken(user);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ..._user } = user;
+
+    return {
+      user: _user,
+      accessToken: token,
+    };
+  }
+
+  private async generateToken(user: UserEntity): Promise<string> {
+    const payload: PayloadType = {
+      sub: user.id,
+      roles: [user.role],
+    };
+
+    const token = this.jwtService.sign(payload);
+
+    return token;
   }
 }
