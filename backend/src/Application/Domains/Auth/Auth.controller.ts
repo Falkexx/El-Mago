8451682ce32f4792ed63bbb74785d20574225288
thalles-victor <thalles.victor2012@gmail.com>
2024-11-12 17:41:58 -0300

    :sparkles: feat: create jwt auth guard.

--- backend/src/Application/Domains/Auth/Auth.controller.ts
+++ backend/src/Application/Domains/Auth/Auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from '../User/dtos/CreateUser.dto';
 import { AuthService } from './Auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller({ path: 'auth', version: '1' })
 export class AuthController {
@@ -15,5 +16,11 @@ export class AuthController {
   @Post('signIn')
   async signIn(@Body() authDto: AuthDto) {
     return this.authService.sinIn(authDto);
+  }
+
+  @Get('protected')
+  @UseGuards(JwtAuthGuard)
+  async protected() {
+    return 'congratulations, you have accessed';
   }
 }
