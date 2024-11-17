eaf66fbce35ae9e4c9e802a4b9311a892c58df7f
thalles-victor <thalles.victor2012@gmail.com>
2024-11-17 10:29:30 -0300

    :bug: fix: the affiliate entity repository declaration.

--- backend/src/Application/Infra/Repositories/Repositories.module.ts
+++ backend/src/Application/Infra/Repositories/Repositories.module.ts
@@ -3,10 +3,11 @@ import { UserTypeOrmRepository } from './UserRepository/UserTypeOrm.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/Application/Entities/User.entity';
 import { AffiliateEntity } from 'src/Application/Entities/Affiliate.entity';
+import { AffiliateTypeOrmRepository } from './AffiliateRepository/AffiliateTypeOrm.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity, AffiliateEntity])],
-  providers: [UserTypeOrmRepository],
+  providers: [UserTypeOrmRepository, AffiliateTypeOrmRepository],
   exports: [TypeOrmModule],
 })
 export class RepositoriesModule {}
