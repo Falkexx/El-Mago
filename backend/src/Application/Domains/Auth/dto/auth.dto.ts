2da38a92081830bb0bfcb6099f22a8235e54abf6
thalles-victor <thalles.victor2012@gmail.com>
2024-11-17 08:21:07 -0300

    :bug: fix: regex password size of 15 to 100.

--- backend/src/Application/Domains/Auth/dto/auth.dto.ts
+++ backend/src/Application/Domains/Auth/dto/auth.dto.ts
@@ -9,7 +9,7 @@ export class AuthDto {
   @IsString()
   @IsNotEmpty()
   @IsNotEmpty()
-  @Matches(/^(?=.*[A-Z])(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-z]).{8,50}$/, {
+  @Matches(/^(?=.*[A-Z])(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-z]).{8,100}$/, {
     message:
       'A senha deve ter 8 carecteres, um especial um maúsculo e um número no mínimo',
   })
