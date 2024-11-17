4b0a905fc69f6f3da255443a4800d6e72388a620
thalles-victor <thalles.victor2012@gmail.com>
2024-11-17 09:23:00 -0300

    :label: type: create api response type.

--- backend/src/@types/index.ts
+++ backend/src/@types/index.ts
@@ -15,3 +15,15 @@ export type PayloadType = {
 };
 
 export type Auth = { id: string };
+
+export interface ApiResponse<T> {
+  status: number; // Código de status HTTP (200, 400, 500, etc.)
+  message: string; // Mensagem amigável (mensagem de sucesso ou erro)
+  data: T; // Dados retornados pela API (pode ser qualquer tipo genérico)
+  meta?: {
+    total?: number; // Número total de itens (usado para paginação)
+    page?: number; // Página atual (usado para paginação)
+    per_page?: number; // Itens por página (usado para paginação)
+  };
+  href?: string;
+}
