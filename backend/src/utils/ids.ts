1d8d5dd0b350bba6e2d97817431c8f4a11ca4cb1
thalles-victor <thalles.victor2012@gmail.com>
2024-11-17 09:21:44 -0300

    :sparkles: feat: create functions to generate ids.

---
+++ backend/src/utils/ids.ts
@@ -0,0 +1,12 @@
+import { v4 } from 'uuid';
+import ShortUniqueId from 'short-unique-id';
+
+const ShortId = new ShortUniqueId();
+
+export function uuidV4() {
+  return v4();
+}
+
+export function shortId(size: number = 10) {
+  return ShortId.rnd(size);
+}
