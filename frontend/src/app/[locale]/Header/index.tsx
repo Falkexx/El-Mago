21b846437b3964215308d9728b59ff447bff67f4
AndreSilvazm <andre.mani2015@gmail.com>
2024-12-04 15:21:29 -0300

    :truck: Chore: Criado página Buttons e adicionado componentes de buttons nela.

--- frontend/src/app/[locale]/Header/index.tsx
+++ frontend/src/app/[locale]/Header/index.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import Logo from "@/midias/logo.svg";
 
 import { Link } from "@/i18n/routing";
-import CurrencyBtn from "@/app/[locale]/components/CurrencyBtn";
-import MyAccountBtn from "@/app/[locale]/components/MyAccountBtn";
-import ShoppingCartBtn from "@/app/[locale]/components/ShoppingCartBtn";
+import CurrencyBtn from "@/app/[locale]/components/Buttons/CurrencyBtn";
+import MyAccountBtn from "@/app/[locale]/components/Buttons/MyAccountBtn";
+import ShoppingCartBtn from "@/app/[locale]/components/Buttons/ShoppingCartBtn";
 
 function Header() {
 
