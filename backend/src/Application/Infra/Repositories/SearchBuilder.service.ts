7bb014b8d429d471ba70a14c168849939aebba8e
thalles-victor <thalles.victor2012@gmail.com>
2025-01-02 15:45:52 -0300

    :sparkles: feat: create search builder service.

---
+++ backend/src/Application/Infra/Repositories/SearchBuilder.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@nestjs/common';
+import { TABLE } from 'src/@metadata/tables';
+import { GenericPaginationDto } from 'src/utils/validators';
+import { SelectQueryBuilder } from 'typeorm';
+
+export type SearchConfig = {
+  searchField?: string;
+  createdField?: string;
+};
+
+@Injectable()
+export class SearchBuilderService {
+  constructor() {}
+
+  async search<Entity>(
+    paginationDto: GenericPaginationDto,
+    tableName: TABLE,
+    queryBuilder: SelectQueryBuilder<Entity>,
+    config?: SearchConfig,
+  ) {
+    const { page, limit, search, filters, order } = paginationDto;
+
+    if (search && config.createdField) {
+      queryBuilder.andWhere(
+        `SIMILARITY(${tableName}.${config.searchField}, :search) > 0.3`,
+        {
+          search: `%${search}%`,
+        },
+      );
+    }
+
+    if (filters) {
+      Object.entries(filters).forEach(([key, value]) => {
+        queryBuilder.andWhere(`${tableName}.${key} = :${key}`, {
+          [key]: value,
+        });
+      });
+    }
+
+    console.log(tableName);
+
+    if (config.createdField) {
+      queryBuilder.orderBy(`"${config.createdField}"`, 'DESC');
+    }
+
+    const [data, total] = await queryBuilder
+      .take(limit)
+      .skip((page - 1) * limit)
+      .getManyAndCount();
+
+    return {
+      data,
+      page,
+      limit,
+      total,
+      order,
+    };
+  }
+
+  generateValidFilters<Entity>(entity: Entity, filters: Record<string, any>) {}
+}
