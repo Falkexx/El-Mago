9c935b215a562fdd7142230a5617fd753512c261
AndreSilvazm <andre.mani2015@gmail.com>
2025-01-08 02:48:10 -0300

    :sparkles: Feat: Criado componente para quando a pessoa não estiver autorizada (afiliados)

---
+++ frontend/src/app/[locale]/affiliate/page.tsx
@@ -0,0 +1,16 @@
+import Affiliate_Unauthorized from '@/app/[locale]/components/Ui/Unauthorized/Affiliate_Unauthorized'
+function page() {
+
+  return <section>
+
+    <main>
+
+
+      <section className='px-32'>
+        <Affiliate_Unauthorized/>
+      </section>
+    </main>
+  </section>;
+}
+
+export default page;
---
+++ frontend/src/app/[locale]/components/Ui/Unauthorized/Affiliate_Unauthorized.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Witcher_Standed from "@/midias/witcher_Standed.png"
+import { Link } from "@/i18n/routing";
+import { useTranslations } from "next-intl";
+function index() {
+
+  const t = useTranslations("Unauthorized_affiliates")
+
+  return (  
+
+    <section className="flex flex-row gap-10 items-center">
+
+        <div>
+          <Image src={Witcher_Standed} alt="Witcher_Standed" className="w-60 h-60"/>
+        </div>
+
+        <article className="flex flex-col gap-10">
+          <h1 className="text-3xl font-bold leading-9">{t("title")}</h1>
+
+          <div className="flex flex-row gap-6">
+            <Link href="/" className="h-11 ctaBtn">{t("siginBtn")}</Link>
+
+            <Link href="/" className="h-11 ctaBtn_WithoutBg">{t("signupBtn")}</Link>
+          </div>
+        </article>
+
+    </section>
+  );
+}
+
+export default index;
