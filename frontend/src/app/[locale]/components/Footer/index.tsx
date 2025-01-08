0e1b1af2e59bb2710749337cdcc8f5a40d2577b6
AndreSilvazm <andre.mani2015@gmail.com>
2025-01-08 02:13:34 -0300

    :lipstick:

--- frontend/src/app/[locale]/components/Footer/index.tsx
+++ frontend/src/app/[locale]/components/Footer/index.tsx
@@ -4,7 +4,7 @@ function Footer(){
 
   return(
 
-    <footer className="w-full flex flex-row gap-8 items-center bg-[#111111] px-32 py-5">
+    <footer className="w-full flex flex-row gap-8 items-center bg-[#111111] px-32 py-5 fixed bottom-0 border-t border-[#343A40]">
       <Image src={Logo} alt="Logo"/>
       <span> © 2024 El Mago/</span>
     </footer>
