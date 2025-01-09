7cb9df7678aaf4570b69f112ff159769c7714531
AndreSilvazm <andre.mani2015@gmail.com>
2025-01-09 15:18:33 -0300

    :truck: Chore | Criado a pasta "Home" na pasta de components para adicionar todos os componentes que estão na página Home.

--- frontend/src/app/[locale]/components/Home/AffiliateSection/index.tsx
+++ frontend/src/app/[locale]/components/Home/AffiliateSection/index.tsx
@@ -1,9 +1,8 @@
-import BecomeAffiliateBtn from "../Ui/Buttons/BecomeAffiliateBtn";
+import BecomeAffiliateBtn from "../../Ui/Buttons/BecomeAffiliateBtn";
 import { useTranslations } from "next-intl";
 
 function index() {
-
-  const t = useTranslations("Affiliates")
+  const t = useTranslations("Affiliates");
 
   return (
     <section className="w-full flex flex-row my-auto gap-16 text-whiteDef">
@@ -11,15 +10,10 @@ function index() {
 
       <article className="w-1/2 flex flex-col justify-between ">
         <article className="w-full h-full flex flex-col gap-4">
-          <h1 className="text-5xl font-bold leading-[48px]">
-            {t("title")}
-          </h1>
+          <h1 className="text-5xl font-bold leading-[48px]">{t("title")}</h1>
 
           <div className="text-base font-normal leading-normal tracking-tight">
-            <p>
-              {t("description")}
-            </p>
-
+            <p>{t("description")}</p>
           </div>
         </article>
         <BecomeAffiliateBtn />
