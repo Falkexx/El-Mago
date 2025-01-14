e48aac83929df6570af527194285a2b6fcf682cf
AndreSilvazm <andre.mani2015@gmail.com>
2025-01-14 11:46:20 -0300

    :sparkles: feat: adicionando os orvelays a tela de Afiliados

--- frontend/src/app/[locale]/affiliate/page.tsx
+++ frontend/src/app/[locale]/affiliate/page.tsx
@@ -1,4 +1,7 @@
 import Operation_steps from "../components/Affiliate/Operation_steps";
+import FinishOrder from "../components/Ui/Overlay/FinishOrder";
+import OrderOverlay from "../components/Ui/Overlay/OrderOverlay";
+import RegisterMarketplace from "../components/Ui/Overlay/RegisterMarketplage";
 
 function page() {
 
@@ -7,6 +10,12 @@ function page() {
     <main>
       <section className='px-32'>
         <Operation_steps/>
+
+        <OrderOverlay affiliateComission={8.90} battleTag="aiaiaiaiai#69" characterName="Japalol" classGame="items > Legendary" order="Tearveil Amulet of Plains Power" orderID={3} plataform="PC" server="Eternal - Hardcore" totalValue={22.22} key={1} />
+
+        <FinishOrder affiliateComission={8.90} orderID={3} totalValue={22.22} key={1}/>
+
+        <RegisterMarketplace/>
       </section>
     </main>
   </section>;
