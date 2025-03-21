fb7f0959283aac65fb7fd35ca9b2ed05f91077d2
thalles-victor <thalles.victor2012@gmail.com>
2025-03-21 17:13:19 -0300

    :construction: feat: send email with pay order.

--- backend/src/Application/Infra/Jobs/Consumer/Payment.consumer.ts
+++ backend/src/Application/Infra/Jobs/Consumer/Payment.consumer.ts
@@ -1,11 +1,22 @@
 import { Processor, WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
-import { KEY_OF_JOB, KEY_OF_QUEUE } from 'src/@metadata/keys';
+import { KEY_INJECTION, KEY_OF_JOB, KEY_OF_QUEUE } from 'src/@metadata/keys';
 import { PaypalWebHookService } from '../../Payment/Paypal/Paypal.webhook.service';
+import { NodemailerService } from '../../Mail/Nodemailer/Nodemailer.service';
+import { IOrderRepositoryContract } from '../../Repositories/OrderRepository/IOrderRepository.contract';
+import { Inject, NotFoundException } from '@nestjs/common';
+import { IUserRepositoryContract } from '../../Repositories/UserRepository/IUserRepository.contract';
 
 @Processor(KEY_OF_QUEUE.PAYMENT)
 export class JobConsumerService extends WorkerHost {
-  constructor(private readonly paypalWebHookService: PaypalWebHookService) {
+  constructor(
+    private readonly paypalWebHookService: PaypalWebHookService,
+    private readonly mailService: NodemailerService,
+    @Inject(KEY_INJECTION.ORDER_REPOSITORY)
+    private readonly orderRepository: IOrderRepositoryContract,
+    @Inject(KEY_INJECTION.USER_REPOSITORY_CONTRACT)
+    private readonly userRepository: IUserRepositoryContract,
+  ) {
     super();
   }
 
@@ -20,5 +31,32 @@ export class JobConsumerService extends WorkerHost {
   private async confirmPayment(data: { body: any; headers: any }) {
     const { body, headers } = data;
     await this.paypalWebHookService.webhookResult(body, headers);
+
+    const orderId = body.resource.purchase_units[0].invoice_id;
+
+    const order = await this.orderRepository.getBy({ id: orderId });
+
+    if (!order) {
+      throw new NotFoundException('order not found, order id: ', order.id);
+    }
+
+    const user = await this.userRepository.getBy({ id: order.id });
+
+    // ATUALIZAR O STATUS DA ORDEM PARA PAGO
+
+    await this.mailService.send({
+      emails: [user.email],
+      name: user.firstName,
+      subject: `confirmação do pagamento`,
+      text: `obrigado por tem comprado ${order.name}`,
+      htmlContent: `
+        <h1>confirmação do pagamento</h1>
+        <h3>${user.firstName} ficamos agradecidos pela compra</h3>
+        <p>${order.name}<p/>
+        <span>id: ${order.id}</span>
+        <span>total: R$ ${order.totalPrice}</span>
+        <span>criado em: R$ ${order.createdAt}</span>
+      `,
+    });
   }
 }
--- backend/src/Application/Infra/Jobs/Job.module.ts
+++ backend/src/Application/Infra/Jobs/Job.module.ts
@@ -1,16 +1,18 @@
 import { BullModule } from '@nestjs/bullmq';
-import { BullMetadataAccessor } from '@nestjs/bullmq/dist/bull-metadata.accessor';
 import { Global, Module } from '@nestjs/common';
-import { KEY_OF_QUEUE } from 'src/@metadata/keys';
+import { KEY_INJECTION, KEY_OF_QUEUE } from 'src/@metadata/keys';
 import { MailModule } from '../Mail/Mail.module';
 import { JobProducerService } from './Producer/Payment.producer';
 import { JobConsumerService } from './Consumer/Payment.consumer';
-import { PaypalWebHookService } from '../Payment/Paypal/Paypal.webhook.service';
 import { PaymentModule } from '../Payment/Payment.module';
+import { RepositoriesModule } from '../Repositories/Repositories.module';
+import { OrderTypeOrmRepository } from '../Repositories/OrderRepository/OrderTypeOrm.repository';
+import { UserTypeOrmRepository } from '../Repositories/UserRepository/UserTypeOrm.repository';
 
 @Global()
 @Module({
   imports: [
+    RepositoriesModule,
     //Implement module of the service
     BullModule.registerQueue({
       name: KEY_OF_QUEUE.PAYMENT,
@@ -18,7 +20,18 @@ import { PaymentModule } from '../Payment/Payment.module';
     MailModule,
     PaymentModule,
   ],
-  providers: [JobProducerService, JobConsumerService],
+  providers: [
+    {
+      provide: KEY_INJECTION.ORDER_REPOSITORY,
+      useClass: OrderTypeOrmRepository,
+    },
+    {
+      provide: KEY_INJECTION.USER_REPOSITORY_CONTRACT,
+      useClass: UserTypeOrmRepository,
+    },
+    JobProducerService,
+    JobConsumerService,
+  ],
   exports: [JobProducerService],
 })
 export class JobsModule {}
