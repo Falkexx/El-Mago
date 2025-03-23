6070dbea610419439049e5ead8068cebcc88ad93
AndreSilvazm <andre.mani2015@gmail.com>
2025-03-22 21:40:26 -0300

    :card_file_box: Data | Adicionadno os dados mockados para servidor

---
+++ frontend/src/app/data/services/mocked/gold/servers_datas.ts
@@ -0,0 +1,16 @@
+import { server_datas_dto } from "@/app/utils/types/gold/servers_datas_dto"
+
+export default function Servers_datas(){
+
+  const data:server_datas_dto = [
+    
+    {title:"Season 6 - Softcore", type:"server"},
+    {title:"Season 6 - Hardcore", type:"server"},
+    {title:"Eternal - Softcore", type:"server"},
+    {title:"Eternal - Hardcore", type:"server"}
+
+  ]
+
+  return data
+
+}
