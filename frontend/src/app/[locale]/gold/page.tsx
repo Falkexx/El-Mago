9e0dbf211d345f923feafdb771e1121a6273fe74
AndreSilvazm <andre.mani2015@gmail.com>
2025-03-22 21:41:20 -0300

    :recycle: refactor | Feito um map para pegar os dados de servidor.

--- frontend/src/app/[locale]/gold/page.tsx
+++ frontend/src/app/[locale]/gold/page.tsx
@@ -1,19 +1,43 @@
+import { getTranslations } from "next-intl/server";
+
 import SelectionBtn from "../components/Ui/Buttons/SelectionBtn";
 import WitcherHead from "../components/Ui/Cards/WitcherHead";
+import Servers_datas from "@/app/data/services/mocked/gold/servers_datas";
 
-function page() {
+async function Page() {
+
+  const t = await getTranslations("Gold")
+
   return (
-    <section className='px-32'>
+    <section className="px-32">
       <main>
-        <WitcherHead title="Buy Diablo 4 Gold" description="Buy Diablo IV Gold For Season/Eternal - Softcore/Hardcore servers from D4Gold.com is your best bet. Get cheapest D4 SC/HC Gold with 24/7 live support and great discounts. Any secure payments methods and free Diablo 4 beginners'guides are available for you at will."/>
+        <WitcherHead
+          title="Buy Diablo 4 Gold"
+          description="Buy Diablo IV Gold For Season/Eternal - Softcore/Hardcore servers from D4Gold.com is your best bet. Get cheapest D4 SC/HC Gold with 24/7 live support and great discounts. Any secure payments methods and free Diablo 4 beginners' guides are available for you at will."
+        />
       </main>
 
-      <section className="w-full flex flex-row">
-        <SelectionBtn id={0} selected={false} title="testing" type="server" value={100} key={0} />
-        <SelectionBtn id={0} selected={true} title="testing" type="server" value={100} key={0} />
+      <section>
+
+        <h1>
+          {t("Server.title")}
+        </h1>
+
+        <section className="w-full flex flex-row">
+          {Servers_datas().map((e, i) => (
+            <SelectionBtn
+              id={i}
+              selected={false} // Estado inicial sempre false, controlado no client
+              title={e.title}
+              type={e.type as "server" | "quantity"}
+              value={e.title}
+              key={i}
+            />
+          ))}
+        </section>
       </section>
     </section>
   );
 }
 
-export default page;
+export default Page;
