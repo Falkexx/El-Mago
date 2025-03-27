072df12c4cef706cc963d73e720b2128a0da468b
AndreSilvazm <andre.mani2015@gmail.com>
2025-03-27 01:07:17 -0300

    :sparkles: Feat | Adicionado as tipagens e dados mockados

---
+++ frontend/src/app/data/services/mocked/gold/quantity_datas.ts
@@ -0,0 +1,30 @@
+import { quantity_datas_dto } from "@/app/utils/types/gold/quantity_datas_dto"
+
+export function Quantity_datas(){
+
+  const data:quantity_datas_dto = [
+
+    {title:"2000 M", type:"quantity", value: 2000},
+    {title:"3000 M", type:"quantity", value: 2000},
+    {title:"4000 M", type:"quantity", value: 2000},
+    {title:"5000 M", type:"quantity", value: 2000},
+    {title:"6000 M", type:"quantity", value: 2000},
+    {title:"7000 M", type:"quantity", value: 2000},
+    {title:"8000 M", type:"quantity", value: 2000},
+    {title:"9000 M", type:"quantity", value: 2000},
+    {title:"10000 M", type:"quantity", value: 2000},
+    {title:"20000 M", type:"quantity", value: 2000},
+    {title:"30000 M", type:"quantity", value: 2000},
+    {title:"40000 M", type:"quantity", value: 2000},
+    {title:"50000 M", type:"quantity", value: 2000},
+    {title:"60000 M", type:"quantity", value: 2000},
+    {title:"70000 M", type:"quantity", value: 2000},
+    {title:"80000 M", type:"quantity", value: 2000},
+    {title:"90000 M", type:"quantity", value: 2000},
+    {title:"100000 M", type:"quantity", value: 2000},
+
+
+  ]
+
+  return data
+}
---
+++ frontend/src/app/utils/types/gold/quantity_datas_dto.ts
@@ -0,0 +1,7 @@
+interface dto {
+  title: string
+  type: "quantity"
+  value: number
+}
+
+export type quantity_datas_dto = dto[] // Um array de dto diretamente
