ca1b2072170f2a70fcb4daeb1473eedd9394ec87
AndreSilvazm <andre.mani2015@gmail.com>
2025-04-22 00:16:28 -0300

    :label: feat: Adicionado a tipagem do next-auth

---
+++ frontend/src/types/next-auth.d.ts
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+    user?: {
+      id?: string;
+      name?: string;
+      email?: string;
+    };
+  }
+
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+    token: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string;
+  }
+}
